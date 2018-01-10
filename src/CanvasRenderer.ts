895d6504d72df94d5c7bedf47a6c3e1a1464dbf0
hikipuro <hikipuro@hotmail.com>
2018-01-10 10:59:14 +0900

    add variable fps function

--- src/CanvasRenderer.ts
+++ src/CanvasRenderer.ts
@@ -1,9 +1,15 @@
 export class CanvasRenderer {
+	public fps: number = 60;
 	public isPaused: boolean = false;
 	public handler: (time: number) => boolean;
 	protected _element: HTMLCanvasElement;
 	protected _context: CanvasRenderingContext2D;
 	protected _requestAnimationFrameHandle: number = 0;
+
+	protected _prevTime: number = 0;
+	protected _prevTimeDiff: number = 0;
+	protected _fpsCount: number = 0;
+	protected _elapsed: number = 0;
 
 	constructor(width: number, height: number) {
 		this._element = document.createElement("canvas");
@@ -12,6 +18,7 @@ export class CanvasRenderer {
 		this._context = this._element.getContext("2d");
 		this._context.imageSmoothingEnabled = false;
 		this._context.webkitImageSmoothingEnabled = false;
+		this.handler = (time: number): boolean => { return false; };
 	}
 
 	public get element(): HTMLCanvasElement {
@@ -27,6 +34,7 @@ export class CanvasRenderer {
 	}
 
 	public start(): void {
+		this._prevTime = performance.now();
 		this._requestAnimationFrameHandle = requestAnimationFrame(this.update);
 	}
 	
@@ -73,8 +81,33 @@ export class CanvasRenderer {
 			this._requestAnimationFrameHandle = requestAnimationFrame(this.update);
 			return;
 		}
-		if (this.handler != null) {
+		if (this.fps === 60) {
 			this.handler(time);
+			this._requestAnimationFrameHandle = requestAnimationFrame(this.update);
+			return;
+		}
+
+		const now = performance.now();
+		let diff = now - this._prevTime;
+		const frameTime = 1000 / this.fps;
+		//console.log(now - this._prevTime, 1000 / this.fps);
+		if (diff + this._prevTimeDiff <= frameTime - 2) {
+			this._requestAnimationFrameHandle = requestAnimationFrame(this.update);
+			return;
+		}
+		this._prevTime += diff;
+		this._elapsed += diff;
+		diff +=  this._prevTimeDiff;
+		while (diff > 0) {
+			this.handler(time);
+			diff -= frameTime;
+			this._fpsCount++;
+		}
+		this._prevTimeDiff = diff;
+		if (this._elapsed > 1000) {
+			this._elapsed -= 1000;
+			//console.log("fps", this._fpsCount);
+			this._fpsCount = 0;
 		}
 		this._requestAnimationFrameHandle = requestAnimationFrame(this.update);
 	}
